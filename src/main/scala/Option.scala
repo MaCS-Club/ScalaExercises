72b8e1432319f3c715bf7d5c6015dc64b2a8e396
avasyukhin <safian.malk@gmail.com>
2017-11-04 19:01:16 +0400

    Added  map2 exercise

--- src/main/scala/Option.scala
+++ src/main/scala/Option.scala
@@ -12,8 +12,6 @@ sealed trait Option[+A] {
 
   def flatMap[B](f: A => Option[B]): Option[B] = ???
 
-  def flatMap_1[B](f: A => Option[B]): Option[B] = ???
-
   def orElse[B>:A](ob: => Option[B]): Option[B] = ???
 
   def filter(f: A => Boolean): Option[A] = ???
@@ -31,6 +29,11 @@ case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
 object Option {
+  //Write the same function using the `for`-comprehension
+  //Make a unit test to prove that this functions are same
+  def map2[A,B,C](a:Option[A],b:Option[B])(f:(A,B)=>C):Option[C]=
+    a flatMap (aa => b map (bb => f(aa,bb)))
+
   def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
 
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
